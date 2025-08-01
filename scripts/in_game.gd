6446c61268af8fe9bd56ffd4b1b7c33f0b438949
dydxcalibur <103492174+dydxcalibur@users.noreply.github.com>
2025-08-01 19:14:24 +0800

    spawn notes according json file
    1/8/2025
    the project can finally follow the beatmap json file to make spawn notes with dictionary data type

--- scripts/in_game.gd
+++ scripts/in_game.gd
@@ -6,21 +6,21 @@ extends Node2D
 
 var beatmap_index = 0
 
-var time_arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
 func _ready():
 	beatmap_loader.load_beatmap("res://beatmaps/0.json")
 	print("Loaded beatmap: ", beatmap_loader.beatmap)
-
+	for note_data in beatmap_loader.beatmap:
+		print("Time:", note_data["time"], " Lane:", note_data["lane"])
 	audio_player.play()
 	#spawner.spawn_note(1)
 
 var rng = RandomNumberGenerator.new()
 func _process(delta):
 	var current_time = audio_player.get_playback_position()
-	if beatmap_index < len(time_arr):
-		if current_time >= time_arr[beatmap_index]:
-			var lane_number = rng.randi_range(0, 3)
-			spawner.spawn_note(lane_number)
-			if beatmap_index <= len(time_arr)-1:
-				beatmap_index += 1
+	while beatmap_index < beatmap_loader.beatmap.size():
+		var note_data = beatmap_loader.beatmap[beatmap_index]
+		if current_time >= note_data["time"]:
+			spawner.spawn_note(note_data["lane"])
+			beatmap_index += 1
+		else:
+			break
